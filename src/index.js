b393efa9657772e87f827d5a9cbdada1d8dab4ae
jose server <jose@gobernacion.com>
2024-07-17 10:11:07 -0400

    express cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import "dotenv/config";
 import { ApolloServer } from "@apollo/server";
 import { makeExecutableSchema } from "@graphql-tools/schema";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const ALTER = true;
 const FORCE = false;
+const PORT = process.env.PORT || 4000;
+const NODE_ENV = process.env.NODE_ENV || "development";
 
 // Models DataBase
 import models from "./models/index";
@@ -16,31 +23,56 @@ import { resolvers } from "./graphql/resolversDef";
 // Schema
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-const serverApollo = new ApolloServer({
-  schema,
-  introspection: process.env.NODE_ENV !== "production",
-});
+const middleware = async (req, res, next) => {
+  if(req.method === "OPTIONS"){
+    res.set("Access-Control-Max-Age", "3600");
+    res.status(204).send();
+    return;
+  }
+  next();
+};
 
-startStandaloneServer(serverApollo, {
-  context: async ({ req, res }) => {
-    // Get the user token from the headers.
+// Start server express
+const app = express();
 
-    //const token = req.headers.authorization || "";
+// Handles hhtpserver
+const httpServer = http.createServer(app);
 
-    // Try to retrieve a user with the token
+const StartServer = async () => {
+  const serverApollo = new ApolloServer({
+cors: false,    
+schema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    introspection: NODE_ENV !== "production",
+  });
 
-    //const user = await getUser(token);
+  await serverApollo.start();
+  // Set up our Express middleware to handle CORS, body parsing,
+  // and our expressMiddleware function.
+  app.use(
+    "/",
+middleware,
+    bodyParser.json({ limit: "50mb" }),
+    expressMiddleware(serverApollo, {
+      context: async ({ req, res }) => {
+        // Get the user token from the headers.
 
-    // optionally block the user
+        //const token = req.headers.authorization || "";
 
-    // we could also check user roles/permissions here
+        // Try to retrieve a user with the token
 
-    //if (!user) // para manejar este bloqueo lo mejor es por key, si no tiene la apikey no puede hacer nada, y la autorizacion por sesión se hace dentro del resolver
-    // throwing a `GraphQLError` here allows us to specify an HTTP status code,
+        //const user = await getUser(token);
 
-    // standard `Error`s will have a 500 status code by default
+        // optionally block the user
 
-    /*throw new GraphQLError("User is not authenticated", {
+        // we could also check user roles/permissions here
+
+        //if (!user) // para manejar este bloqueo lo mejor es por key, si no tiene la apikey no puede hacer nada, y la autorizacion por sesión se hace dentro del resolver
+        // throwing a `GraphQLError` here allows us to specify an HTTP status code,
+
+        // standard `Error`s will have a 500 status code by default
+
+        /*throw new GraphQLError("User is not authenticated", {
         extensions: {
           code: "UNAUTHENTICATED",
 
@@ -48,46 +80,31 @@ startStandaloneServer(serverApollo, {
         },
       });*/
 
-    return {
-      models: models,
-    };
-  },
-  listen: { port: process.env.PORT || 4000 },
-})
-  .then(({ url }) => {
-    models.sequelizeInst
-      .sync({ alter: ALTER, force: FORCE })
-      .then(() => {
-        console.log(
-          `Running on ${url} (${process.env.NODE_ENV}) with introspection(${
-            process.env.NODE_ENV !== "production"
-          })`
-        );
-      })
-      .catch((e) => {
-        console.error(`error sync sequelize ${e}`);
-      });
-  })
-  .catch((e) => {
-    console.error(`error startStandaloneServer graphql ${e}`);
-  });
+        return {
+          models: models,
+        };
+      },
+    })
+  );
 
-/*startStandaloneServer(serverApollo, {
-  context: async ({ req, res }) => ({
-    models: models,
-  }),
-})
-  .then(({ url }) => {
-    sequelize
-      .sync({ force: true, alter: true })
-      .then(() => {
-        console.log(`Sincronización completa de la base de datos`);
-      })
-      .catch((e) => {
-        console.error(`error sync sequelize ${e}`);
-      });
-  })
-  .catch((e) => {
-    console.error(`error startStandaloneServer graphql ${e}`);
-  });
-*/
+  new Promise((resolve) => httpServer.listen({ port: PORT }, resolve))
+    .then(() => {
+      models.sequelizeInst
+        .sync({ alter: ALTER, force: FORCE })
+        .then(() => {
+          console.log(
+            `Running in express on http://localhost:${PORT} (${NODE_ENV}) with introspection(${
+              NODE_ENV !== "production"
+            })`
+          );
+        })
+        .catch((e) => {
+          console.error(`error sync sequelize ${e}`);
+        });
+    })
+    .catch((e) => {
+      console.error(`error startStandaloneServer graphql ${e}`);
+    });
+};
+
+StartServer();
